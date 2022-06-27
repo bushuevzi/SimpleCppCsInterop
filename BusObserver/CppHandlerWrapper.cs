84a8d2387dc6632cf597581508a8daaa7a75eece
Zakhar Bushuev <z.bushuev@sdventures.com>
2022-06-27 13:32:09 +0300

    incapsulate c++ invoking in wrapper

---
+++ BusObserver/CppHandlerWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BusObserver
+{
+    public static class CppHandlerWrapper
+    {
+        [DllImport("..\\..\\..\\..\\x64\\Debug\\CppHandler.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double Add(double a, double b);
+
+        [DllImport("..\\..\\..\\..\\x64\\Debug\\CppHandler.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double Subtract(double a, double b);
+
+        [DllImport("..\\..\\..\\..\\x64\\Debug\\CppHandler.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double Multiply(double a, double b);
+
+        [DllImport("..\\..\\..\\..\\x64\\Debug\\CppHandler.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern double Divide(double a, double b);
+    }
+}
--- BusObserver/Program.cs
+++ BusObserver/Program.cs
@@ -1,17 +1,25 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 
 namespace BusObserver
 {
     class Program
     {
-        [DllImport("..\\..\\..\\..\\x64\\Debug\\CppHandler.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern double Add(double a, double b);
-
         static void Main(string[] args)
         {
-            double result = Add(1.0, 3.0);
-            Console.WriteLine(result);
+            var a = 1.0;
+            var b = 3.0;
+
+            var result = CppHandlerWrapper.Add(a, b);
+            Console.WriteLine($"a + b = {result}");
+
+            result = CppHandlerWrapper.Subtract(a, b);
+            Console.WriteLine($"a - b = {result}");
+
+            result = CppHandlerWrapper.Multiply(a, b);
+            Console.WriteLine($"a * b = {result}");
+
+            result = CppHandlerWrapper.Divide(a, b);
+            Console.WriteLine($"a / b = {result}");
         }
     }
 }
